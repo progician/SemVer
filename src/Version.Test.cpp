14bdeb5464e910c68bba9344923bb308ada31fc6
Gyula Gubacsi <gyula.gubacsi@gmail.com>
2019-02-14 19:45:28 +0000

    Use const qualifiers as these operations should not change any operands

--- src/Version.Test.cpp
+++ src/Version.Test.cpp
@@ -2,11 +2,17 @@
 #include "SemVer/Version.h"
 
 TEST_CASE("Semantic versions can be compared") {
-  SemVer::Version lower_precedent{1, 0, 0};
-  SemVer::Version higher_precedent{2, 0, 0};
+  SemVer::Version const lower_precedent{1, 0, 0};
+  SemVer::Version const higher_precedent{2, 0, 0};
   REQUIRE(lower_precedent < higher_precedent);
 
   SECTION("the opposite must be false") {
     REQUIRE_FALSE(higher_precedent < lower_precedent);
+  }
+
+  SECTION("equality is if all the main digits are equal") {
+    SemVer::Version const one_of_the_same{1, 2, 3};
+    SemVer::Version const another_one_of_the_same{1, 2, 3};
+    REQUIRE(one_of_the_same == another_one_of_the_same);
   }
 }
